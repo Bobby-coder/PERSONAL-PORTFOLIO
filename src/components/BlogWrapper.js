b11a7f3956a2f32d50d5b833400179ed7f46abda
Bobby-coder <bobbysadhwani612@gmail.com>
2022-10-11 00:05:17 +0530

    blogWrapper.css imported

--- src/components/BlogWrapper.js
+++ src/components/BlogWrapper.js
@@ -1,14 +1,17 @@
 import Blog from './Blog'
+import './BlogWrapper.css'
 import blogApi from './blogApi'
 
 const BlogWrapper = () =>{
     return(
         <>
+        <div className='blog-wrapper'>
         {
             blogApi.map((currObj) => {
                 return <Blog title = {currObj.title} liveLink = {currObj.liveLink} hashnodeLink = {currObj.hashnodeLink} imgSrc = {currObj.imgSrc} />
             })
         }
+        </div>
         </>
     )
 }
