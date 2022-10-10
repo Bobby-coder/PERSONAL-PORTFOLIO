942f8cd6db8ec2662d0bb5c276cfd2e9e328edbf
Bobby-coder <bobbysadhwani612@gmail.com>
2022-10-10 17:42:05 +0530

    project api added

---
+++ src/components/projectApi.js
@@ -0,0 +1,45 @@
+const projectApi = [
+    {
+        title: 'AMAZON CLONE',
+        liveLink: "https://clone-ff7af.web.app",
+        githubLink: "https://github.com/Bobby-coder/Amazon-Clone",
+        imgSrc: `/images/amazon-clone.png`
+    },
+
+    {
+        title: 'RANDOM QUOTE GENERATOR',
+        liveLink: "https://random-quote-inventor.netlify.app",
+        githubLink: "https://github.com/Bobby-coder/Random-Quote-Generator",
+        imgSrc: `/images/random-quote-generator.png`
+    },
+    
+    {
+        title: 'RODE UI CLONE',
+        liveLink: "https://rode-web.netlify.app",
+        githubLink: "https://github.com/Bobby-coder/RODE-UI-CLONE",
+        imgSrc: `/images/rode-clone.webp`
+    },
+
+    {
+        title: 'PAYTM UI CLONE',
+        liveLink: "https://paytm-web.netlify.app",
+        githubLink: "https://github.com/Bobby-coder/PAYTM-UI-CLONE",
+        imgSrc: `/images/paytm-clone.webp`
+    },
+
+    {
+        title: 'SHOPIFY UI CLONE',
+        liveLink: "https://shopify-web.netlify.app",
+        githubLink: "https://github.com/Bobby-coder/SHOPIFY-UI-CLONE",
+        imgSrc: `/images/shopify-clone.webp`
+    },
+
+    {
+        title: 'VISUAL DESIGNER PORTFOLIO',
+        liveLink: "https://product-design-webpage.netlify.app",
+        githubLink: "https://github.com/Bobby-coder/VISUAL-DESIGN-LANDING-PAGE",
+        imgSrc: `/images/Project-15.webp`
+    },
+]
+
+export default projectApi;
