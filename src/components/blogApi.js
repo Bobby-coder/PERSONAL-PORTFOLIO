61666ed5b8037ef539fc62fa1765a8af36773422
Bobby-coder <bobbysadhwani612@gmail.com>
2022-10-10 18:18:02 +0530

    blogApi added

---
+++ src/components/blogApi.js
@@ -0,0 +1,17 @@
+const blogApi = [
+    {
+        title: 'JAVASCRIPT ARRAY AND ITS METHODS',
+        liveLink: 'https://bobby-sadhwani.hashnode.dev/javascript-array-handbook',
+        hashnodeLink: 'https://bobby-sadhwani.hashnode.dev',
+        imgSrc: '/images/blog-1.webp'
+    },
+
+    {
+        title: 'JAVASCRIPT OBJECTS',
+        liveLink: 'https://bobby-sadhwani.hashnode.dev/javascript-objects-handbook',
+        hashnodeLink: 'https://bobby-sadhwani.hashnode.dev',
+        imgSrc: '/images/blog-2.png'
+    },
+]
+
+export default blogApi;
